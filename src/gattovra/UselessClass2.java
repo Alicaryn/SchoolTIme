fa78f25b74249bdf1d747437ee6b3e27e2c05c3d
FSEVERI\dorado3349 <FSEVERI\dorado3349@lmatbi-02.fseveri.it>
2016-02-23 09:01:37 +0100

    new pointless class

---
+++ src/gattovra/UselessClass2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gattovra;
+
+/**
+ *
+ * @author FSEVERI\dorado3349
+ */
+public class UselessClass2 {
+    
+}
