05d721468b74d1714486d3d9a2872c1f4fac87a3
FSEVERI\bergamasco3075 <FSEVERI\bergamasco3075@lmatbi-01.fseveri.it>
2016-02-23 09:16:52 +0100

    Another Initializated project

--- src/gattovra/Gattovra.java
+++ src/gattovra/Gattovra.java
@@ -18,6 +18,7 @@ public class Gattovra {
         // TODO code application logic here
         System.out.println("Hello World");
         System.out.println("Hello World Again");
+        System.out.println("Moriarty was here");
     }
     
 }
