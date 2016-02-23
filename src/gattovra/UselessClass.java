f5be3caf3339d479003b4eaf42f9668a93b3eeda
FSEVERI\dorado3349 <FSEVERI\dorado3349@lmatbi-02.fseveri.it>
2016-02-23 08:54:28 +0100

    initializated project

---
+++ src/gattovra/UselessClass.java
@@ -0,0 +1,14 @@
+package gattovra;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author FSEVERI\dorado3349
+ */
+public class UselessClass {
+    
+}
